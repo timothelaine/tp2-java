ae1672d49b9e778ca5466a2f298bd4490c8a3360
Timothe LAINE <contact@timothelaine.com>
2020-07-02 14:58:19 +0200

    add of main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        public static void main(String [] args){
+            Client c1 = new Client(1,"Martin");
+            Client c2= new Client(2,"Dupond");
+            Compte com1 = new Compte(10,c1);
+            Compte com2 = new Compte(20,c2);
+            System.out.println(com1);
+            System.out.println(com2);
+        }
     }
 }
