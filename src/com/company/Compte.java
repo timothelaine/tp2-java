6e2d65e6fa6862bdeedcf6b6a8fc887ed503f4bd
Telougat <thelorenzo533@gmail.com>
2020-07-02 14:50:49 +0200

    Add some methods to Compte class

--- src/com/company/Compte.java
+++ src/com/company/Compte.java
@@ -6,23 +6,24 @@ public class Compte {
     private float solde;
 
     public Compte(long numero, Client c) {
-
+        this.numero = numero;
+        this.client = c;
     }
 
     public float getSolde() {
-        return 123;
+        return this.solde;
     }
 
     public Client getClient() {
-        return new Client(1, "23");
+        return this.client;
     }
 
     public long getNumero() {
-        return 123;
+        return this.numero;
     }
 
     public String toString() {
-        return new String("");
+        return new String("Compte " + this.numero + this.client.toString() + " - Solde : " + this.solde);
     }
 
     public void main(String [] args) {
