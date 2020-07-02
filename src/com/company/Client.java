50969e7f249643e59ba8b0b4299650c7c15a8631
Telougat <thelorenzo533@gmail.com>
2020-07-02 14:25:14 +0200

    Skeleton

---
+++ src/com/company/Client.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Client {
+    private long numero;
+    private String nom;
+
+    public Client(long numero, String nom) {
+
+    }
+
+    public long getNumero() {
+        return 123;
+    }
+
+    public String getNom() {
+        return new String("");
+    }
+
+    public void setNom(String nom) {
+
+    }
+
+    public String toString() {
+        return new String("");
+    }
+}
