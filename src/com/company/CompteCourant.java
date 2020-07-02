253a4c72ef41b97986a13b52679cf2242724504e
Timothe LAINE <contact@timothelaine.com>
2020-07-02 15:37:00 +0200

    Add CompteCourrant class

---
+++ src/com/company/CompteCourant.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class CompteCourant extends Compte{
+
+    public CompteCourant(long numero, Client c) {
+        super(numero, c);
+    }
+
+    public String toString() {
+        return new String("Compte courant no: " + this.getNumero() + " - Solde : " + this.getSolde());
+    }
+}
