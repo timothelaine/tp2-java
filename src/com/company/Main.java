9262073e9b59d5d0566f2e9fcce0eea9cad7a7f6
Timothe LAINE <contact@timothelaine.com>
2020-07-02 15:14:05 +0200

    correction

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,11 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-            Client c1 = new Client(1,"Martin");
-            Client c2= new Client(2,"Dupond");
-            Compte com1 = new Compte(10,c1);
-            Compte com2 = new Compte(20,c2);
-            System.out.println(com1);
-            System.out.println(com2);
+
     }
 }
